7e7d6a80b5f8f62f64d1bb7ecf3899e53ac4aa56
Rene Dussault <63034012+ReneDussault@users.noreply.github.com>
2023-08-04 20:21:21 -0400

    fix needed cause of API update

--- ZeddyBot.py
+++ ZeddyBot.py
@@ -29,30 +29,55 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-# checks if discord activity is streaming, if true, assign LIVE role, if False, remove LIVE role
+CHANNEL_ID = 966493808869138442
 LIVE_ROLE_ID = 983061320133922846
 
 
-@bot.event
-async def on_member_update(before, after):
-    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------ZeddyBot is checking for roles----------")
+@loop(hours=24 * 5)  # Fetch a new token every 5 days (adjust as needed)
+async def update_token_task():
+    acc_tok = get_app_access_token()
+    print(
+        f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------Changing access token----------"
+    )
+    config_data["access_token"] = acc_tok
+    with open("config.json", "w") as f:
+        json.dump(config_data, f)
+    await bot.change_presence(status=discord.Status.online)  # Refresh bot status
+
+
+# checks if discord user's activity is "streaming" on twitch, if true, assign LIVE role, if False, remove LIVE role
+@bot.listen("on_member_update")
+@bot.listen("on_presence_update")
+async def update_live_role(before, after):
+    print(
+        f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------ZeddyBot is checking for roles----------"
+    )
     if any(a for a in after.activities if a.type == discord.ActivityType.streaming):
         if LIVE_ROLE_ID in after._roles:
             return
         else:
-            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------ZeddyBot is Giving LIVE role to {after.name}----------")
+            print(
+                f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------ZeddyBot is Giving LIVE role to {after.name}----------"
+            )
             await after.add_roles(after.guild.get_role(LIVE_ROLE_ID))
 
     else:
         if LIVE_ROLE_ID in after._roles:
-            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------ZeddyBot is Removing LIVE role from {after.name}----------")
+            print(
+                f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------ZeddyBot is Removing LIVE role from {after.name}----------"
+            )
             await after.remove_roles(after.guild.get_role(LIVE_ROLE_ID))
 
 
 # print that we logged in once the bot is ready
 @bot.event
 async def on_ready():
-    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------ZeddyBot is connected to Discord----------")
+    print(
+        f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------ZeddyBot is connected to Discord----------"
+    )
+
+    update_token_task.start()  # Start the token updating background task
+    check_twitch_online_streamers.start()  # Start the Twitch stream checking task
 
 
 # ping command
@@ -68,19 +93,21 @@ async def hello(ctx):
 
 
 # post in discord if streamer is live
-@loop(seconds=30)
+@loop(seconds=60)
 async def check_twitch_online_streamers():
-    channel = bot.get_channel(966493808869138442)
+    channel = bot.get_channel(CHANNEL_ID)
 
     if not channel:
         return
 
     notifications = get_notifications()
     for notification in notifications:
-        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------Sending discord notification----------")
+        print(
+            f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------Sending discord notification----------"
+        )
         await channel.send(
             f"We are now live! {notification['title']}.\nStreaming: {notification['game_name']} at https://www.twitch.tv/renegadezed\n"
-            )
+        )
 
 
 """
@@ -92,7 +119,6 @@ twitch stuff below
 
 # oath access token
 def get_app_access_token():
-    
     params = {
         "client_id": config_data["twitch_client_id"],
         "client_secret": config_data["twitch_secret"],
@@ -105,48 +131,23 @@ def get_app_access_token():
     return access_token
 
 
-access_token = get_app_access_token()
-config_data['access_token'] = access_token
-# print(access_token)
-
-
 # convert config.json watchlist names to appropriate twitch login names and IDs
 def get_users(login_names):
-
     params = {"login": login_names}
-
-    if config_data['access_token'] != access_token:
-
-        acc_tok = get_app_access_token()
-
-        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------Changing access token----------")
-        config_data['access_token'] = acc_tok
-    
-        with open("config.json", "w") as f:
-            json.dump(config_data, f)
 
     headers = {
         "Authorization": f"Bearer {config_data['access_token']}",
         "Client-Id": config_data["twitch_client_id"],
     }
 
-    response = rq.get("https://api.twitch.tv/helix/users", params=params, headers=headers)
+    response = rq.get(
+        "https://api.twitch.tv/helix/users", params=params, headers=headers
+    )
     return {entry["login"]: entry["id"] for entry in response.json()["data"]}
 
 
 # get stream info when live and reformat to a dictionary
 def get_streams(users):
-
-    if config_data['access_token'] != access_token:
-
-        acc_tok = get_app_access_token()
-
-        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}----------Changing access token----------")
-        config_data['access_token'] = acc_tok
-
-        with open("config.json", "w") as f:
-            json.dump(config_data, f)
-
     params = {"user_id": users.values()}
 
     headers = {
@@ -154,7 +155,9 @@ def get_streams(users):
         "Client-Id": config_data["twitch_client_id"],
     }
 
-    response = rq.get("https://api.twitch.tv/helix/streams", params=params, headers=headers)
+    response = rq.get(
+        "https://api.twitch.tv/helix/streams", params=params, headers=headers
+    )
     return {entry["user_login"]: entry for entry in response.json()["data"]}
 
 
@@ -185,5 +188,4 @@ def get_notifications():
 
 
 if __name__ == "__main__":
-    check_twitch_online_streamers.start()
     bot.run(disc_token)
